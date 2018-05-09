4495c033c1725b8705e842c4334b4248cf74fe20
Cullen Taylor <cullentaylor@outlook.com>
2018-05-09 17:18:59 -0500

    Modify make-image.sh dir naming convention
    Signed-off-by: Cullen Taylor <cullentaylor@outlook.com>

--- deploy/images/make-image.sh
+++ deploy/images/make-image.sh
@@ -12,7 +12,7 @@ src_dir=$(realpath $images_dir/../..)
 build_dir=$(mktemp -d)
 trap "rm -rf $build_dir" EXIT
 
-cp -rp $src_dir $build_dir/rotisserie
-cp $dockerfile $build_dir/rotisserie/Dockerfile
+cp -rp $src_dir $build_dir/wrike-updater
+cp $dockerfile $build_dir/wrike-updater/Dockerfile
 
-sudo docker build $build_dir/rotisserie -t $image_name
+docker build $build_dir/wrike-updater -t $image_name
